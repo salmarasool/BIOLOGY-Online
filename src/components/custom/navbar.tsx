1e9d405c9adaeea21975276dedf5c37f20d458bb
salmarasool <salmarasooll@gmail.com>
2023-12-27 13:16:43 +0500

    commit changes

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -5,12 +5,12 @@
  
 function Navbar(){
     return(
-<div className="flex flex-row-[10%, 80%, 10%] place-items-center justify-around bg-white min-h-[64px]">
+<div className="flex flex-row-[10%, 80%, 10%] place-items-center justify-center bg-white min-h-[64px]">
   <div>
     <Image src={logo} className=" h-20 w-20 rounded-full" alt="Logo"/>
   </div>
   <div className="hidden sm:flex">
-    <Menu />
+    <Menu/>
   </div>
 </div>
     )
